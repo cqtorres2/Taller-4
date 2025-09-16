b3d86fa825f5f55bcbda52ab1e59a7991bdf57f3
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-09-15 22:24:37 -0500

    final

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,12 @@
 /*public class Prueba {
+private int numero1;
+private int numero2;
+
     public static void main(String[] args) {
         System.out.println("La velocidad es: " + velocidadMaxima);
+     public prueba(){
+     int numero1 = 23;
+     int numero2 = 4;
+     }   
     }
 }*/
